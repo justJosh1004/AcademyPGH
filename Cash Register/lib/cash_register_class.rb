3b2f0add901f649a959e358d4b04ea45ca88fb56
Josh <j.joson@live.com>
2017-03-14 22:24:04 -0400

    cash register works up to 100 dollars

--- Cash Register/lib/cash_register_class.rb
+++ Cash Register/lib/cash_register_class.rb
@@ -1,14 +1,14 @@
 class Cash_register
   def initialize
-    @bill = ['$10', '$5', '$1']
-    @bill_value = [10, 5, 1]
-    @bill_amount = [0, 0, 0]
+    @bill = ['$100', '$50', '$20', '$10', '$5', '$1']
+    @bill_value = [100, 50, 20, 10, 5, 1]
+    @bill_amount = [0, 0, 0, 0, 0, 0]
   end
 
   def make_change(change)
 
     while change > 0
-      if change > 10
+      if change >= 100
         bill_index = 0
         while change >= @bill_value[bill_index]
           @bill_amount[bill_index] += 1
@@ -16,8 +16,32 @@ class Cash_register
         end
         bill_index += 1
 
-      elsif change > 5
-        bill_index = 0
+      elsif change >= 50
+        bill_index = 1
+        while change >= @bill_value[bill_index]
+          @bill_amount[bill_index] += 1
+          change -= @bill_value[bill_index]
+        end
+        bill_index += 1
+
+      elsif change >= 20
+        bill_index = 2
+        while change >= @bill_value[bill_index]
+          @bill_amount[bill_index] += 1
+          change -= @bill_value[bill_index]
+        end
+        bill_index += 1
+
+      elsif change >= 10
+        bill_index = 3
+        while change >= @bill_value[bill_index]
+          @bill_amount[bill_index] += 1
+          change -= @bill_value[bill_index]
+        end
+        bill_index += 1
+
+      elsif change >= 5
+        bill_index = 4
         while change >= @bill_value[bill_index]
           @bill_amount[bill_index] += 1
           change -= @bill_value[bill_index]
@@ -25,23 +49,14 @@ class Cash_register
         bill_index += 1
 
       elsif change > 0
-        bill_index = 1
+        bill_index = 5
         while change >= @bill_value[bill_index]
           @bill_amount[bill_index] += 1
           change -= @bill_value[bill_index]
         end
-        next if change == 0
       end
 
     end
     return @bill_amount
-    #return "#{@bill_amount[bill_index]} - #{@bill[bill_index]}"
-=begin
-    while change >= 1
-      @money_amount[1] += 1
-      change -= 1
-    end
-    return "#{@money_amount[1]} - #{@money[1]}"
-=end
   end
 end
--- Cash Register/spec/cash_register_spec.rb
+++ Cash Register/spec/cash_register_spec.rb
@@ -5,49 +5,98 @@ describe "Cash_register" do
     context "given a change of $1" do
       it "returns 1 $1 bill" do
         change = Cash_register.new
-        expect(change.make_change(1)).to eql([0, 1])
+        expect(change.make_change(1)).to eql([0, 0, 0, 0, 0, 1])
       end
     end
 
     context "given a change of $4" do
       it "returns 4 $1 bills" do
         change = Cash_register.new
-        expect(change.make_change(4)).to eql([0, 4])
+        expect(change.make_change(4)).to eql([0, 0, 0, 0, 0, 4])
       end
     end
 
     context "given a change of $5" do
       it "returns 1 $5 bill" do
         change = Cash_register.new
-        expect(change.make_change(5)).to eql([0, 1, 0])
+        expect(change.make_change(5)).to eql([0, 0, 0, 0, 1, 0])
       end
     end
 
     context "given a change of $6" do
       it "returns 1 $5 bill and 1 $1 bill" do
         change = Cash_register.new
-        expect(change.make_change(6)).to eql([0, 1, 1])
-      end
-    end
-
-    context "given a change of $7" do
-      it "returns 1 $5 bill and 2 $1 bill" do
-        change = Cash_register.new
-        expect(change.make_change(7)).to eql([0, 1, 2])
+        expect(change.make_change(6)).to eql([0, 0, 0, 0, 1, 1])
       end
     end
 
     context "given a change of $9" do
       it "returns 1 $5 bill and 4 $1 bill" do
         change = Cash_register.new
-        expect(change.make_change(9)).to eql([0, 1, 4])
+        expect(change.make_change(9)).to eql([0, 0, 0, 0, 1, 4])
       end
     end
 
     context "given a change of $10" do
       it "returns 1 $10 bill" do
         change = Cash_register.new
-        expect(change.make_change(10)).to eql([1, 0, 0])
+        expect(change.make_change(10)).to eql([0, 0, 0, 1, 0, 0])
+      end
+    end
+
+    context "given a change of $12" do
+      it "returns 1 $10 bill and 2 $2 bills" do
+        change = Cash_register.new
+        expect(change.make_change(12)).to eql([0, 0, 0, 1, 0, 2])
+      end
+    end
+
+    context "given a change of $18" do
+      it "returns 1 $10 bill, 1 $5 bill, and 3 $2 bills" do
+        change = Cash_register.new
+        expect(change.make_change(18)).to eql([0, 0, 0, 1, 1, 3])
+      end
+    end
+
+    context "given a change of $20" do
+      it "returns 1 $20 bill" do
+        change = Cash_register.new
+        expect(change.make_change(20)).to eql([0, 0, 1, 0, 0, 0])
+      end
+    end
+
+    context "given a change of $37" do
+      it "returns 1 $30 bill, 1 $10 bill, 1 $5 bill, and 2 $2 bills" do
+        change = Cash_register.new
+        expect(change.make_change(37)).to eql([0, 0, 1, 1, 1, 2])
+      end
+    end
+
+    context "given a change of $48" do
+      it "returns 2 $20 bills, 1 $5 bill, and 3 $2 bills" do
+        change = Cash_register.new
+        expect(change.make_change(48)).to eql([0, 0, 2, 0, 1, 3])
+      end
+    end
+
+    context "given a change of $50" do
+      it "returns 1 $50 bill" do
+        change = Cash_register.new
+        expect(change.make_change(50)).to eql([0, 1, 0, 0, 0, 0])
+      end
+    end
+
+    context "given a change of $99" do
+      it "returns 1 $50 bill, 2 $20 bills, 1 $5 bill, and 4 $1 bills" do
+        change = Cash_register.new
+        expect(change.make_change(99)).to eql([0, 1, 2, 0, 1, 4])
+      end
+    end
+
+    context "given a change of $388" do
+      it "returns 3 $100 bills, 1 $50 bill, 1 $20 bill, 1 $10 bill, 1 $5 bill, 3 $1 bills" do
+        change = Cash_register.new
+        expect(change.make_change(388)).to eql([3, 1, 1, 1, 1, 3])
       end
     end
 
